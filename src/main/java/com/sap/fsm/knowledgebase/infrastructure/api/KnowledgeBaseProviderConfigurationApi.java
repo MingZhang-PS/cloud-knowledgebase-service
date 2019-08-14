0de07f534eb8fae081ca85c0297066cd539f80b6
Ming Zhang <ming.zhang02@sap.com>
2019-08-14 19:59:10 +0800

    catch up master latest

--- src/main/java/com/sap/fsm/knowledgebase/infrastructure/api/KnowledgeBaseProviderConfigurationApi.java
+++ src/main/java/com/sap/fsm/knowledgebase/infrastructure/api/KnowledgeBaseProviderConfigurationApi.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.http.MediaType;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
 
 import io.swagger.annotations.*;
 
